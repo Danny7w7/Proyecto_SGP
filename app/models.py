37aea6de28e1e74d04fd682330e98f916455750f
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2023-08-28 14:22:05 -0500

    add new models to databases

--- app/models.py
+++ app/models.py
@@ -1,6 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class Roles(models.Model):
+    administrador = models.BooleanField()
+    formulador = models.BooleanField()
+    evaluador = models.BooleanField()
+    lector = models.BooleanField()
+
+class Usuarios(AbstractUser):
+    cc_nit = models.IntegerField()
+    roles = models.ManyToManyField(Roles)
+
+    def __str__(self):
+        return self.username
+
+class Proyecto(models.Model):
+    codigo_Grupo_Investigacion = models.IntegerField(null=False)
+    nombre_Grupo_Investigacion = models.CharField(max_length=100, null=False)
+    linea_Grupo_Investigacion = models.CharField(max_length=100, null=False)
+    titulo_Proyecto = models.CharField(max_length=100, null=False)
+    red_Conocimiento_Sectorial = models.CharField(max_length=100, null=False)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE) 
+
+class Conocimientos(models.Model):
+    areas_Conocimiento = models.CharField(max_length=100, null=False)
+    subareas_Conocimiento = models.CharField(max_length=100, null=False)
+    diciplinas_Subarea = models.CharField(max_length=100, null=False)
+    proyecto = models.OneToOneField(Proyecto, on_delete=models.CASCADE)
+
+class Informacion_Proyecto(models.Model):
+    codigo_Dependencia_Presupuestal = models.CharField(max_length=10, null=False)
+    actividades_economicas_del_proyecto_investigacion = models.CharField(max_length=100, null=False)
+    tematicas_Estrategias_SENA = models.CharField(max_length=100, null=False)
+    link_video_proyecto = models.CharField(max_length=500, null=False)
+    proyecto_Relacionado_Industrial40 = models.BooleanField(null=False)
+    justificacion_Industrial = models.CharField(max_length=500, null=True)
+    proyecto_Relacionado_Economia_Naranja = models.BooleanField(null=False)
+    justificacion_Economia_Naranja = models.CharField(max_length=500, null=True)
+    proyecto_Relacionado_Politica_Discapacidad = models.BooleanField(null=False)
+    justificacion_Politica_Discapacidad = models.CharField(max_length=500, null=True)
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE) 
 
 class Objetivos(models.Model):
     objetivo_general = models.CharField(max_length=250, null=False)
@@ -53,7 +94,7 @@ class Entidades_aliadas(models.Model):
     descripcion_destinacion_dinero_aportado = models.CharField(max_length=150, null=True)
     nombre_grupo_inv_entidad_aliada = models.CharField(max_length=170, null=True)
     codigo_gruplac_entidad_aliada = models.IntegerField(null=True)
-    link_gruplac_entidad_aliada = models.CharField(null=True)
+    link_gruplac_entidad_aliada = models.CharField(max_length=500 ,null=True)
     actividades_desarrollar_entidad_aliada_marco_proyecto = models.CharField(max_length=250, null=True)
     objetivo_especificos_relacionados = models.CharField(max_length=250, null=True)
     metodologia_act_transferencia_centro_formacion = models.CharField(max_length= 250, null=True)
@@ -81,18 +122,18 @@ class Autores(models.Model):
     Numero_Cedula_Autor = models.IntegerField(null=False)
     Email_Autor_Proyecto = models.EmailField(null=False)
     Numero_Telefono_Autor = models.IntegerField(null=False)
-    Tipo_Vinculacion_entidad = models.CharField(null=False, choices=[('planta', 'Planta'), ('contratista', 'Contratista') , ('planta_temporal' , 'Planta_temporal')])
+    Tipo_Vinculacion_entidad = models.CharField(max_length=15 ,null=False, choices=[('planta', 'Planta'), ('contratista', 'Contratista') , ('planta_temporal' , 'Planta_temporal')])
     Rol_Sennova_De_Participantes_de_Proyecto = models.CharField(max_length=50, choices=[('formulador', 'Formulador'), ('investigador', 'Investigador'), ('dinamizador', 'Dinamizador'), ('instructor', 'Instructor'), ('aprendiz','Aprendiz')], null=True)
     Numero_meses_vinculacion_Autor = models.IntegerField(null=False)
     Numero_horas_Semanales_dedicadas_Autores = models.IntegerField(null=False)
     
 class Analisis_Participantes(models.Model):
     Nombre_Semillero_Investigacion_Beneficiados = models.CharField(null=False, max_length=100)
-    Numero_Programas_Beneficiarios_Conforman_Semilleros_Investigacion = models.IntegerField(null=False)
+    Numero_Programas_Beneficiarios_Semilleros_Investigacion = models.IntegerField(null=False)
     Tipo_programas_formación_beneficiados_conforman_semillero = models.CharField(null=False, max_length=100)
     Nombre_programas_formación_beneficiados_semillero = models.CharField(null=False, max_length=100)
     Tipo_programas_de_formación_beneficiados_por_ejecución = models.CharField(null=True, max_length=100)
     Nombre_programas_formación_beneficiados_ejecución_proyecto = models.CharField(null=True, max_length=100)
     Número_aprendices_participarán_ejecución_proyecto = models.IntegerField(null=False)
     Número_municipios_beneficiados = models.IntegerField(null=False)
-    Nombre__municipios_beneficiados_descripción_beneficio = models.CharField(null=False, max_length=200)
+    Nombre_municipios_beneficiados_descripción_beneficio = models.CharField(null=False, max_length=200)
