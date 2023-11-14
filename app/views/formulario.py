b4c96011b5d33e1b64adf4da6c8c15c22b7722bf
Juan David Cova Salgado <99416813+JuanDCova@users.noreply.github.com>
2023-11-14 15:28:44 -0500

    Corrección de modelos y anexos
    Se implemento el guardado de documentos y también se le dio un limite de 5MB máximo por documento

--- app/views/formulario.py
+++ app/views/formulario.py
@@ -270,10 +270,11 @@ def subir_anexos(request, proyecto_id):
             anexo = form.save(commit=False)
             anexo.proyecto = proyecto
             anexo.save()
+
     documents = Document.objects.filter(proyecto=proyecto)
-    contex = {'docs':documents,
-              'proyecto':proyecto,
-            'percentaje':proyecto_id}
+    contex = {'docs': documents,
+              'proyecto': proyecto,
+              'percentaje': proyecto_id}
     return render(request, "form/anexos.html", contex)
 
 
