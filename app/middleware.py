15a8c1830bb36052c2a6df7049f2b33330645ae3
MiguelMH55 <est.miguelmanrique11a@gmail.com>
2023-12-11 11:35:28 -0500

    Update 'continuar-session'

--- app/middleware.py
+++ app/middleware.py
@@ -3,11 +3,26 @@ from django.utils.deprecation import MiddlewareMixin
 from .models import UltimaVista
 
 class ActualizarUltimaVistaMiddleware(MiddlewareMixin):
-    EXCLUIDAS = ['/continuar-sesion/', '/register/', '/logout/', '/recuperar-contrasena/', '/proyectos_usuario/' , 
-                 '/edit_proyec/<int:id_proyecto>/','/editar_objetivo/<int:id_proyecto>/','/Dashboard/', '/Dashboard/Doc-Anexos/', 
-                 '/Dashboard/Usuarios/', '/Dashboard/PreguntasPoli',
-                 '/Dashboard/Proyectos-Inactivos/','/Dashboard/Proyectos-Completos/','/Dashboard/Proyectos-Inactivos/',
-                 '/Dashboard/Proyectos-Pendientes/','/Dashboard/404/notfount/']
+    EXCLUIDAS = [
+
+    #Administrador y privadas
+    '/continuar-sesion/', '/register/', '/logout/', '/recuperar-contrasena/',
+    #Proyecto
+    '/proyectos_usuario/' , '/edit_proyec/<int:id_proyecto>/','/editar_objetivo/<int:id_proyecto>/',
+    '/editar_anexo/<int:proyecto_id>/',
+    '/eliminar_usuario/<int:usuario_id>/',
+
+    #Formulador
+    '/Dashboard/', '/Dashboard/Doc-Anexos/', '/Dashboard/Usuarios/', '/Dashboard/PreguntasPoli',
+    '/Dashboard/Proyectos-Inactivos/','/Dashboard/Proyectos-Completos/','/Dashboard/Proyectos-Inactivos/',
+    '/Dashboard/Proyectos-Pendientes/','/Dashboard/404/notfount/' 
+
+    #Anexo
+    '/editar_anexo/<int:proyecto_id>/',
+    # PDF
+        '/generar_pdf/<int:proyecto_id>/',
+        '/generar_c_valor/<int:proyecto_id>/',
+    ]
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         if request.user.is_authenticated:
